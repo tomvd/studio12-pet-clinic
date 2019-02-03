ff2f54c158821328880f3a7e03415249c4dd58be
Tom Van Dyck <tomvdtom@gmail.com>
2019-02-03 17:19:50 +0100

    adding pojos. Closes #2.

---
+++ src/main/java/be/studio12/studio12petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package be.studio12.studio12petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/be/studio12/studio12petclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package be.studio12.studio12petclinic.model;
+
+import be.studio12.studio12petclinic.model.Person;
+
+public class Vet extends Person {
+}
