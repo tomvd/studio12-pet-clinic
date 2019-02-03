6da28477c5935f3ea31ce98c630af81bd08a096b
Tom Van Dyck <tomvdtom@gmail.com>
2019-02-03 13:23:51 +0100

    init project

---
+++ src/main/java/be/studio12/studio12petclinic/Studio12PetClinicApplication.java
@@ -0,0 +1,14 @@
+package be.studio12.studio12petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studio12PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Studio12PetClinicApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/be/studio12/studio12petclinic/Studio12PetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package be.studio12.studio12petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Studio12PetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
